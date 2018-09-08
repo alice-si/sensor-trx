16cefec9e50fe91ed145720e424ea4e8432bde78
William Wnekowicz <william.wnekowicz@gmail.com>
2018-09-08 16:02:13 +0200

    feat: material-ui navbar

--- blockchain/src/App.js
+++ blockchain/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
 import { HiddenOnlyAuth, VisibleOnlyAuth } from './util/wrappers.js'
 
 // UI Components
 import LoginButtonContainer from './user/ui/loginbutton/LoginButtonContainer'
 import LogoutButtonContainer from './user/ui/logoutbutton/LogoutButtonContainer'
+
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 // Styles
 import './css/oswald.css'
@@ -12,43 +19,63 @@ import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
 
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+
 class App extends Component {
   render() {
+    const { classes } = this.props
     const OnlyAuthLinks = VisibleOnlyAuth(() =>
       <span>
-        <li className="pure-menu-item">
-          <Link to="/dashboard" className="pure-menu-link">Dashboard</Link>
-        </li>
-        <li className="pure-menu-item">
-          <Link to="/profile" className="pure-menu-link">Profile</Link>
-        </li>
+        <Button color="inherit" href="/dashboard">
+          Dashboard
+        </Button>
+        <Button color="inherit" href="/profile">
+          Profile
+        </Button>
         <LogoutButtonContainer />
       </span>
     )
 
     const OnlyGuestLinks = HiddenOnlyAuth(() =>
       <span>
-        <li className="pure-menu-item">
-          <Link to="/signup" className="pure-menu-link">Sign Up</Link>
-        </li>
+        <Button color="inherit" href="/signup">
+          Sign Up
+        </Button>
         <LoginButtonContainer />
       </span>
     )
 
     return (
       <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-          <ul className="pure-menu-list navbar-right">
+        <AppBar position='static'>
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              SensorTRX
+            </Typography>
             <OnlyGuestLinks />
             <OnlyAuthLinks />
-          </ul>
-          <Link to="/" className="pure-menu-heading pure-menu-link">Truffle Box</Link>
-        </nav>
-
+          </Toolbar>
+        </AppBar>
         {this.props.children}
       </div>
     );
   }
 }
 
-export default App
+export default withStyles(styles)(App)
