1db7ccb5aad3b0f240baa34851e1e0d27dec7520
alexAlice <alex@alice.si>
2018-09-09 04:44:36 +0200

    Sesnors signing implemented

--- sensor/config.js
+++ sensor/config.js
@@ -3,4 +3,8 @@ let Config = {};
 Config.botAccessToken = '679087014:AAHtsvDuJ8w54c3gm54KD_sx-keJ6N24x7Q';
 Config.chatId = '-1001341088791';
 
+Config.privateKey = 'cf749dfd3f6b985ed17acbd528402f28ebfd0bf166da541470ec1876ee963a40';
+
+Config.address = '01db8c011f8caa93479fa6db874e08156c7d02ab'; // It is not used in code
+
 module.exports = Config;
--- sensor/test-send.js
+++ sensor/test-send.js
@@ -1,9 +1,71 @@
 const TelegramSender = require('./telegram-sender');
+const Web3Utils = require('web3-utils');
+const Config = require('./config');
+const ethUtil = require('ethereumjs-util');
 
-TelegramSender.sendMessage('Test first').then(function(res) {
+run();
+
+function run() {
+  const TIME = getTimestamp();
+  const VALUE = getValue();
+
+  const data = Web3Utils.soliditySha3(
+    {
+      type: 'uint8',
+      value: VALUE
+    },
+    {
+      type: 'uint256',
+      value: TIME
+    }
+  );
+
+  const signedData = getSignedData(VALUE, TIME);
+
+  const messageJSON = JSON.stringify(signedData);
+  console.log('Sending following message into Telegram channel:');
+  console.log(messageJSON);
+
+  TelegramSender.sendMessage(messageJSON).then(function(res) {
     console.log("\x1b[32m%s\x1b[0m" ,"Cool");
     console.dir(res);
-}, function (err) {
+  }, function (err) {
     console.log("\x1b[31m%s\x1b[0m" ,"'Not cool :(");
     console.log(err);
-});
+  });
+}
+
+function getSignedData(value, time) {
+  const hash = Web3Utils.soliditySha3(
+    {
+      type: 'uint8',
+      value: value
+    },
+    {
+      type: 'uint256',
+      value: time
+    }
+  );
+
+  var privkey = new Buffer(Config.privateKey, 'hex');
+  var data = ethUtil.hashPersonalMessage(ethUtil.toBuffer(hash));
+  var vrs = ethUtil.ecsign(data, privkey);
+
+  return {
+    v: ethUtil.bufferToHex(vrs.v),
+    r: ethUtil.bufferToHex(vrs.r),
+    s: ethUtil.bufferToHex(vrs.s),
+    msgHash: hash,
+    value: value,
+    time: time,
+    signer: ethUtil.privateToAddress(privkey).toString('hex')
+  };
+}
+
+function getTimestamp() {
+  return (new Date()).valueOf();
+}
+
+function getValue() {
+  return 100; // read from argv first arg
+}
